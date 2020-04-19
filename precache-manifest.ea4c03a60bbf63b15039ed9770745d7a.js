fd775763f51a0b71b05b7883ea05a5d651ff7a1f
fuser312 <sherltonGT40000>
2020-04-19 22:36:20 +0530

    Initial commit

---
+++ precache-manifest.ea4c03a60bbf63b15039ed9770745d7a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74f4b00f86bf0f06eae49b00caea29f7",
+    "url": "/fuser312/quiz_webapp_starter/index.html"
+  },
+  {
+    "revision": "1f553dbbee12c48d187d",
+    "url": "/fuser312/quiz_webapp_starter/static/css/main.deb1e2f5.chunk.css"
+  },
+  {
+    "revision": "b9a75c2c3ae95f168b70",
+    "url": "/fuser312/quiz_webapp_starter/static/js/2.0f1af954.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fuser312/quiz_webapp_starter/static/js/2.0f1af954.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f553dbbee12c48d187d",
+    "url": "/fuser312/quiz_webapp_starter/static/js/main.bdc2f752.chunk.js"
+  },
+  {
+    "revision": "78285c669dd53b96731c",
+    "url": "/fuser312/quiz_webapp_starter/static/js/runtime-main.e52d5597.js"
+  }
+]);
